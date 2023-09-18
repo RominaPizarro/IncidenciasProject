952805e1bd8eaa75e5ec5ea95fc45a5d4b4b6c2e
RominaPizarro <ropizarro87@gmail.com>
2023-09-18 01:56:23 -0500

    Integrando login

--- control_incidencias/core/urls.py
+++ control_incidencias/core/urls.py
@@ -1,5 +1,10 @@
 
 from django.urls import path
 
+from .views import index, login, register
+
 urlpatterns = [
+    path('', index, name="index"),
+    path('login', login, name="login"),
+    path('register', register, name="register"),
 ]
