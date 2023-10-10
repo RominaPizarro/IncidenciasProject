2c6d3f057f560fe8f12db69d0ea781c9e3f3be60
RominaPizarro <ropizarro87@gmail.com>
2023-10-10 00:06:14 -0500

    Configuración para Deploy

--- control_incidencias/control_incidencias/urls.py
+++ control_incidencias/control_incidencias/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('core.urls')),
+    
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
 ]
