ee9b6cfef88e4616e6a8f9f76aa2fd8176e86952
RominaPizarro <ropizarro87@gmail.com>
2023-10-10 00:20:44 -0500

    Upload settings

--- control_incidencias/control_incidencias/settings.py
+++ control_incidencias/control_incidencias/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-p@d#)3q4pw-f!%vx#mb%wl$rb$aiub4*+6=1sto2+o%2%drl$1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'control-incidencias-3cbb7db79687.herokuapp.com/']
 
 
 # Application definition
